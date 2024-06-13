27288ceec2eac54986466eccd25cd8320dd803aa
Arsenii Vladimirov <78355358+grokhi@users.noreply.github.com>
2024-06-13 16:58:01 +0500

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -15,7 +15,7 @@ from dataset_tools.templates import (
 ##################################
 PROJECT_NAME: str = "RTSD: Russian Traffic Sign Images Dataset"
 PROJECT_NAME_FULL: str = "RTSD: Russian Traffic Sign Images Dataset"
-HIDE_DATASET = True  # set False when 100% sure about repo quality
+HIDE_DATASET = False  # set False when 100% sure about repo quality
 
 ##################################
 # * After uploading to instance ##
